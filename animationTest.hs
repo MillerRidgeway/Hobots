55299629139470561e132d803a97c41c71e2c005
Miller Ridgeway <miller.ridgeway@western.edu>
2016-11-28 13:54:03 -0700

    Updated engine with command and RobotState, changed animation

--- animationTest.hs
+++ animationTest.hs
@@ -18,19 +18,19 @@ main = animate window background drawing
 
 drawing :: Float -> Picture
 drawing f = pictures 
-  [ translate (color ballColor $ circleSolid 10
-  , color paddleColor $ rectangleSolid 5 15
+  [ translate f f (color ballColor $ circleSolid 10)
+   ,translate f f (color paddleColor $ rectangleSolid 5 15)
   --, translate (-50) (-50) (color ballColor $ circleSolid 10)
   --, translate (-50) (-50) (color paddleColor $ rectangleSolid 5 15)
   --, translate (-200) (30) (color ballColor $ circleSolid 10)
   --, translate (-200) (30) (color paddleColor $ rectangleSolid 5 15)
   --, translate (-300) (180) (color ballColor $ circleSolid 10)
   --, translate (-300) (180) (color paddleColor $ rectangleSolid 5 15)
-  , translate (-120 + 10 * f) 0 (color boxColor $ rectangleWire 400 400)
-  --, translate 150 150 (color boxColor $ scale 0.25 0.25 $ text "Robot 1")
-  --, translate 150 50 (color boxColor $ scale 0.25 0.25 $ text "Robot 2")
-  --, translate 150 (-50) (color boxColor $ scale 0.25 0.25 $ text "Robot 3")
-  --, translate 150 (-150) (color boxColor $ scale 0.25 0.25 $ text "Robot 4")
+  , translate (-120) 0 (color boxColor $ rectangleWire 400 400)
+  , translate 150 150 (color boxColor $ scale 0.25 0.25 $ text "Robot 1")
+  , translate 150 50 (color boxColor $ scale 0.25 0.25 $ text "Robot 2")
+  , translate 150 (-50) (color boxColor $ scale 0.25 0.25 $ text "Robot 3")
+  , translate 150 (-150) (color boxColor $ scale 0.25 0.25 $ text "Robot 4")
   ]
   where
     ballColor = dark red
--- engine.hs
+++ engine.hs
@@ -2,12 +2,16 @@ module Engine where
 
 --data degree :: Int -- 0-360 
 --data resolution :: Int -- +/- 10 degrees
+--data speed :: Int -- 0-100
 
-data Robot = Robot {x :: Int, y :: Int, damage :: Int, speed :: Int, heading :: Int}
+data RobotState = RobotState {x :: Int, y :: Int, damage :: Int, speed :: Int, heading :: Int}
 data MaxRobots = [Robot]
 
-scan :: Int -> Int -> Int
-scan d r = if r > 10 then 10 else r
+data Command = Scan | Fire | Move 
+	deriving Show 
+
+rScan :: Int -> Int -> Int
+rScan d r = if r > 10 then 10 else r
            --if d > 359 then d = 0 else d = d
            --if d < 0 then d = -d else d = d
 
