b571e2055c5fee34ad23c8ffaa9c9fd08b80e703
Miller Ridgeway <miller.ridgeway@western.edu>
2016-11-30 13:51:12 -0700

    Updated engine

--- engine.hs
+++ engine.hs
@@ -3,17 +3,26 @@ module Engine where
 --data degree :: Int -- 0-360 
 --data resolution :: Int -- +/- 10 degrees
 --data speed :: Int -- 0-100
+type GameState  = [(RobotState, RobotState -> Command)]
 
-data RobotState = RobotState {x :: Int, y :: Int, damage :: Int, speed :: Int, heading :: Int}
-data MaxRobots = [Robot]
+data RobotState = RobotState {x :: Int, y :: Int, damage :: Int, speed :: Int, heading :: Int, scanInfo :: Int}
+data MaxRobots = [RobotState]
 
-data Command = Scan | Fire | Move 
-	deriving Show 
+data Command = Scan Int Int | Fire Int Int | Move Int Int 
+    deriving Show 
+
+commandStep :: RobotState -> Command
+commandStep rState rCommand = 
+
+
+checkStatus :: RobotState -> Bool
+checkStatus r = if r {damage} /= 0 then true
 
 rScan :: Int -> Int -> Int
-rScan d r = if r > 10 then 10 else r
-           --if d > 359 then d = 0 else d = d
-           --if d < 0 then d = -d else d = d
+rScan d r = if r > 10 then r = 10 else r = r
+            if d > 359 then d = 0 else d = d
+            if d < 0 then d = -d else d = d
+            if fold checkStatus MaxRobots then 
 
 
 main :: IO ()
