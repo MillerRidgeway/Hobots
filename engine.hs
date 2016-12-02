3ebfd6d111ae9d46a76b54f3e2656ed07f209be0
Miller Ridgeway <miller.ridgeway@western.edu>
2016-12-01 22:48:31 -0700

    Added bigStep and gameStep, with comments explaining. Also added type signatures for supporting functions, but not defined yet.

--- engine.hs
+++ engine.hs
@@ -1,29 +1,41 @@
 module Engine where
 
---data degree :: Int -- 0-360 
---data resolution :: Int -- +/- 10 degrees
---data speed :: Int -- 0-100
-type GameState  = [(RobotState, RobotState -> Command)]
+type GameState = [RobotState]  
 
-data RobotState = RobotState {x :: Int, y :: Int, damage :: Int, speed :: Int, heading :: Int, scanInfo :: Int}
-data MaxRobots = [RobotState]
+data RobotState = RobotState {x :: Double, 
+                              y :: Double, 
+                              damage :: Int, 
+                              speed :: Double, 
+                              heading :: Double, 
+                              scanInfo :: Double, 
+                              id :: Int,
+                              rStep :: RobotState -> Command} -- rStep is a genaric step 
+															  -- which pushes the robot forward with a specific step when called
 
-data Command = Scan Int Int | Fire Int Int | Move Int Int 
+data Command = Scan Double Double | Fire Double Double | Move Double Double 
     deriving Show 
 
-commandStep :: RobotState -> Command
-commandStep rState rCommand = 
+gameStep :: [RobotState] -> [RobotState]
+gameStep robots = let (explosions, robots') = foldl (bigStep robots) ([],[]) robots in --Define an inital list of robots and explosions
+                       foldl (\robots'' (x,y) -> map (rDamage x y) robots'') robots' explosions --Damage robots with an explosion and produce 
+				                                                                                --new lists of robots and explosions 
 
+bigStep :: [RobotState] -> ([(Double, Double)],[RobotState]) -> RobotState -> ([(Double, Double)],[RobotState]) -- Creates an updated robot state 
+                                                                                                                -- and explosion list by pushing a modified robot
+																												-- the robot is modified by rStep, which is 
+																												-- determined by the function as well
+bigStep allRobots (explosions,newRobots) robot =
+    case (rStep robot) robot of 
+        Scan degree res -> (explosions, rDrive $ rScan degree res robot : newRobots ) -- Scan case, drive (you always drive), scan, and update the lists
+        Fire degree dist -> (rFire degree dist robot : explosions, rDrive robot : newRobots )
+        Drive dir vel -> (explosions, rDrive (setDir dir vel robot) : newRobots )
 
-checkStatus :: RobotState -> Bool
-checkStatus r = if r {damage} /= 0 then true
+setDir :: Double -> Double -> RobotState -- Sets direction and velocity of a given robot in the new state list
 
-rScan :: Int -> Int -> Int
-rScan d r = if r > 10 then r = 10 else r = r
-            if d > 359 then d = 0 else d = d
-            if d < 0 then d = -d else d = d
-            if fold checkStatus MaxRobots then 
+rScan :: Double -> Double -> [RobotState] -> RobotState -> RobotState -- Degree, Variance, All Robots, Current Robot, New Robot from Current
 
+rDrive :: RobotState -> RobotState -- All robots are driving at each step, so we simply move the state forward in terms of location
 
-main :: IO ()
-main = print $ scan 0 8
+rDamage :: Double -> Double -> RobotState -> RobotState -- X coord, Y coord, robot firing, and a damaged robot
+
+rFire :: Double -> Double -> RobotState -> (Double, Double) -- Degree, Distance, robot, location of explosion to be added to explosions
